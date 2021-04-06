56f37d36ab391b7dfe191bdc867ebc337dd6729d
MyNameIsAres <chrisvonk@hotmail.nl>
2021-04-06 12:43:47 +0200

    Start of project BetterClaims! :tada:

---
+++ src/main/java/org/geminicraft/betterclaims/MainPlugin.java
@@ -0,0 +1,12 @@
+package org.geminicraft.betterclaims;
+
+import org.mineacademy.fo.Common;
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public class MainPlugin extends SimplePlugin {
+
+    @Override
+    protected void onPluginStart() {
+        Common.logFramed("BetterPlugins is starting...");
+    }
+}
